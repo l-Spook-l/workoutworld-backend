2c3aa68ee09359687c73588a040c4d3b929918ca
l-Spook-l <uaspookua@gmail.com>
2024-09-18 18:36:32 +0300

    The database connection file has been moved to the core folder

--- src/core/database.py
+++ src/core/database.py
@@ -3,7 +3,7 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.orm import DeclarativeBase
 
-from .config import DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD, DB_USER
+from src.core.config import DB_NAME, DB_PORT, DB_HOST, DB_PASSWORD, DB_USER
 
 DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
