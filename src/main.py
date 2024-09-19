d59113e953ac06690af6ce4567f9a5b401591fc6
l-Spook-l <uaspookua@gmail.com>
2024-09-19 18:51:21 +0300

    Restructured the main file after moving some blocks into separate modules

--- src/main.py
+++ src/main.py
@@ -1,22 +1,36 @@
 import uvicorn
 from fastapi.staticfiles import StaticFiles
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI, Depends
 from src.auth.models import User
-from src.workouts.models import Workout
 from src.auth.base_config import current_user
-from src.workouts.router import router as router_workout
 from src.auth.router import router as router_user
-from src.admin.router import router as router_admin
+from src.workouts.router import router as router_workout
 
-from sqladmin import Admin, ModelView
-from .database import engine
+from src.admin.admin import init_admin
+
+from src.core.redis import lifespan
+from src.core.cors import setup_cors
+from src.core.metrics import setup_metrics
+from src.core.sentry import sentry_sdk
 
 app = FastAPI(
-    title="Workout App"
+    title="Workout App",
+    lifespan=lifespan
 )
 
 app.mount("/api/media", StaticFiles(directory="src/media"), name="media")
+
+# Настройка CORS
+setup_cors(app)
+
+# Настройка Prometheus
+setup_metrics(app)
+
+# Инициализируем админ-панель
+init_admin(app)
+
+app.include_router(router_user, prefix="/api")
+app.include_router(router_workout, prefix="/api")
 
 
 @app.get("/api/protected-route")
@@ -24,47 +38,6 @@ def protected_route(user: User = Depends(current_user)):
     return user
 
 
-app.include_router(router_user, prefix="/api")
-app.include_router(router_workout, prefix="/api")
-app.include_router(router_admin, prefix="/api")
-
-
-# CORS
-origins = [
-    f"http://localhost:3000",
-    f"http://45.137.66.74:3000",
-    f"https://45.137.66.74:3000",
-    f"http://vm4791907.25ssd.had.wf",
-    f"https://vm4791907.25ssd.had.wf",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allow_headers=["*"],
-    # allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers", "Access-Control-Allow-Origin",
-    #                "Authorization"],
-)
-
-
-admin = Admin(app, engine)
-
-
-class UserAdmin(ModelView, model=User):
-    column_list = [User.id, User.username]
-
-
-class WorkoutAdmin(ModelView, model=Workout):
-    column_list = [Workout.id, Workout.name]
-
-
-admin.add_view(UserAdmin)
-admin.add_view(WorkoutAdmin)
-
-
 if __name__ == "__main__":
     # uvicorn src.main:app --reload
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
-
