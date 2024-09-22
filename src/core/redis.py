2432534911a99099c43920d2188771a46ec62bd7
l-Spook-l <uaspookua@gmail.com>
2024-09-22 17:17:44 +0300

    Client retrieval for Redis extracted separately for better interaction with it

--- src/core/redis.py
+++ src/core/redis.py
@@ -6,9 +6,14 @@ from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 
 
+async def get_redis_client():
+    redis = aioredis.from_url("redis://redis_app:6379/0")
+    return redis
+
+
 # для запуска и завершения фоновых задач
 @asynccontextmanager
 async def lifespan(_: FastAPI) -> AsyncIterator[None]:
-    redis = aioredis.from_url("redis://localhost")
+    redis = await get_redis_client()  # Инициализация Redis
     FastAPICache.init(RedisBackend(redis), prefix="fastapi-cache")
     yield
