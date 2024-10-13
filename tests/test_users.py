c4a17675a84d25785f9d1b246f6340e02fc74c4f
l-Spook-l <uaspookua@gmail.com>
2024-10-13 20:42:51 +0300

    Added tests to check for invalid input during new user registration

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,4 @@
+import pytest
 from sqlalchemy import insert, select
 from httpx import AsyncClient
 from src.users.models import Role, User
@@ -49,8 +50,76 @@ async def test_register_user(ac: AsyncClient):
         assert registered_user.first_name == user_data["first_name"], "Имя не совпадает."
         assert registered_user.last_name == user_data["last_name"], "Фамилия не совпадает."
         assert registered_user.phone == user_data["phone"], "Телефон не совпадает."
-        assert registered_user.is_superuser == user_data["is_superuser"], "Статус суперпользователя не совпадает."
-        assert registered_user.is_active == user_data["is_active"], "Статус активности не совпадает."
+        assert registered_user.is_active is True, "Статус активности не совпадает."
+        assert registered_user.is_superuser is False, "Статус суперпользователя не совпадает."
+        assert registered_user.is_verified is False, "Статус активности не совпадает."
+
+
+@pytest.mark.parametrize("user_data, expected_status_code, expected_detail", [
+    ({
+         "email": "",
+         "password": "",
+         "first_name": "",
+         "last_name": "",
+         "phone": ""
+     }, 422, {'detail': [{'type': 'value_error', 'loc': ['body', 'email'],
+                          'msg': 'value is not a valid email address: The email '
+                                 'address is not valid. It must have exactly one @-sign.',
+                          'input': '',
+                          'ctx': {'reason': 'The email address is not valid. It must have exactly one @-sign.'}},
+                         {'type': 'string_too_short', 'loc': ['body', 'password'],
+                          'msg': 'String should have at least 8 characters', 'input': '', 'ctx': {'min_length': 8}},
+                         {'type': 'string_too_short', 'loc': ['body', 'first_name'],
+                          'msg': 'String should have at least 5 characters', 'input': '', 'ctx': {'min_length': 5}},
+                         {'type': 'string_too_short', 'loc': ['body', 'last_name'],
+                          'msg': 'String should have at least 5 characters', 'input': '', 'ctx': {'min_length': 5}}]}),
+    ({
+         "email": "test@example.com",
+         "password": "short",
+         "first_name": "",
+         "last_name": "",
+         "phone": ""
+     }, 422, {'detail': [
+        {'type': 'string_too_short', 'loc': ['body', 'password'], 'msg': 'String should have at least 8 characters',
+         'input': 'short', 'ctx': {'min_length': 8}},
+        {'type': 'string_too_short', 'loc': ['body', 'first_name'], 'msg': 'String should have at least 5 characters',
+         'input': '', 'ctx': {'min_length': 5}},
+        {'type': 'string_too_short', 'loc': ['body', 'last_name'], 'msg': 'String should have at least 5 characters',
+         'input': '', 'ctx': {'min_length': 5}}]}),
+    ({
+         "email": "test@example.com",
+         "password": "valid_password",
+         "first_name": "J",
+         "last_name": "",
+         "phone": ""
+     }, 422, {'detail': [
+        {'type': 'string_too_short', 'loc': ['body', 'first_name'], 'msg': 'String should have at least 5 characters',
+         'input': 'J', 'ctx': {'min_length': 5}},
+        {'type': 'string_too_short', 'loc': ['body', 'last_name'], 'msg': 'String should have at least 5 characters',
+         'input': '', 'ctx': {'min_length': 5}}]}),
+    ({
+         "email": "test@example.com",
+         "password": "valid_password",
+         "first_name": "Oliver",
+         "last_name": "",
+         "phone": ""
+     }, 422, {'detail': [
+        {'type': 'string_too_short', 'loc': ['body', 'last_name'], 'msg': 'String should have at least 5 characters',
+         'input': '', 'ctx': {'min_length': 5}}]}),
+    ({
+         "email": "user1@example.com",
+         "password": "valid_password",
+         "first_name": "Oliver",
+         "last_name": "Johnson",
+         "phone": ""
+     }, 400, {
+         "detail": "REGISTER_USER_ALREADY_EXISTS"
+     })
+])
+async def test_register_user_validation_error(ac: AsyncClient, user_data, expected_status_code, expected_detail):
+    response = await ac.post("/api/users/register", json=user_data)
+    assert response.status_code == expected_status_code
+    assert response.json() == expected_detail
 
 
 async def test_login(ac: AsyncClient):
