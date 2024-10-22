76f09f1df647a1da3826537511d41c5229b05bf7
l-Spook-l <uaspookua@gmail.com>
2024-10-22 18:42:04 +0300

    Added full URLs for the database and test database

--- src/core/config.py
+++ src/core/config.py
@@ -1,5 +1,5 @@
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
 
 load_dotenv()
@@ -12,12 +12,16 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
+DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 DB_USER_TEST = os.environ.get("DB_USER_TEST")
 DB_PASSWORD_TEST = os.environ.get("DB_PASSWORD_TEST")
 DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
 DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
 DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DATABASE_URL_TEST = \
+    f"postgresql+asyncpg://{DB_USER_TEST}:{DB_PASSWORD_TEST}@{DB_HOST_TEST}:{DB_PORT_TEST}/{DB_NAME_TEST}"
+
 
 REDIS_PORT = os.environ.get("REDIS_PORT")
 
