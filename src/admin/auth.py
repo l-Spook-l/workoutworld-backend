43459caeebdef830bb73457d7326b55a458d2380
l-Spook-l <uaspookua@gmail.com>
2024-10-23 20:40:58 +0300

    Added variables for admin panel login and password

--- src/admin/auth.py
+++ src/admin/auth.py
@@ -2,7 +2,7 @@ import jwt
 from datetime import datetime, timedelta, timezone
 from sqladmin.authentication import AuthenticationBackend
 from starlette.requests import Request
-from src.core.config import ADMIN_SECRET_KEY
+from src.core.config import ADMIN_SECRET_KEY, ADMIN_USERNAME, ADMIN_PASSWORD
 
 
 class AdminAuth(AuthenticationBackend):
@@ -10,19 +10,17 @@ class AdminAuth(AuthenticationBackend):
         form = await request.form()
         username, password = form["username"], form["password"]
 
-        # После проверки имени пользователя и пароля
-        payload = {
-            "username": username,  # Можно добавить другие данные, например, роли
-            "exp": datetime.now(timezone.utc) + timedelta(hours=1),  # Время жизни токена
-        }
+        if username == ADMIN_USERNAME and password == ADMIN_PASSWORD:
+            payload = {
+                "username": username,
+                "exp": datetime.now(timezone.utc) + timedelta(hours=1),
+            }
 
-        # Создание токена
-        token = jwt.encode(payload, ADMIN_SECRET_KEY, algorithm="HS256")
-        # Validate username/password credentials
-        # And update session
-        request.session.update({"token": token})
+            token = jwt.encode(payload, ADMIN_SECRET_KEY, algorithm="HS256")
+            request.session.update({"token": token})
+            return True
 
-        return True
+        return False
 
     async def logout(self, request: Request) -> bool:
         # Usually you'd want to just clear the session
