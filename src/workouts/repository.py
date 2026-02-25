b9a6ae43af8d8af9e0e2f07949a6fa1cb9349f4b
l-Spook-l <uaspookua@gmail.com>
2026-02-25 17:56:07 +0200

    refactor session management in ExerciseRepository

--- src/workouts/repository.py
+++ src/workouts/repository.py
@@ -178,50 +178,48 @@ class WorkoutRepository:
 
 
 class ExerciseRepository:
-    @staticmethod
-    async def create_exercise(session: AsyncSession, data):
-        stat = insert(Exercise).values(**data.model_dump(exclude_none=True)).returning(Exercise.id)
-        result = await session.execute(stat)
-        exercise_id = result.scalar()
-        return exercise_id
+    def __init__(self, session: AsyncSession):
+        self.session = session
 
-    @staticmethod
-    async def get_exercise_by_id(session: AsyncSession, exercise_id: int):
-        return await session.get(Exercise, exercise_id)
+    async def create_exercise(self, data) -> int:
+        try:
+            stat = insert(Exercise).values(**data.model_dump(exclude_none=True)).returning(Exercise.id)
+            result = await self.session.execute(stat)
+            exercise_id = result.scalar()
+            return exercise_id
+        except IntegrityError as exc:
+            log.exception(exc)
 
-    @staticmethod
-    async def update_exercise(
-            session: AsyncSession,
-            exercise_id: int,
-            update_data: ExerciseUpdate,
+    async def get_exercise_by_id(self, exercise_id: int):
+        return await self.session.get(Exercise, exercise_id)
 
-    ):
-        query = update(Exercise).filter(Exercise.id == exercise_id).values(**update_data.model_dump(exclude_none=True))
-        await session.execute(query)
+    async def update_exercise(self, exercise_id: int, update_data: ExerciseUpdate):
+        try:
+            query = update(Exercise).filter(Exercise.id == exercise_id).values(
+                **update_data.model_dump(exclude_none=True))
+            await self.session.execute(query)
+        except IntegrityError as exc:
+            log.exception(exc)
 
-    @staticmethod
-    async def get_exercises_by_workout_id(session: AsyncSession, workout_id: int):
-        query = await session.execute(select(Exercise).filter(Exercise.workout_id == workout_id))
+    async def get_exercises_by_workout_id(self, workout_id: int):
+        query = await self.session.execute(select(Exercise).filter(Exercise.workout_id == workout_id))
         exercises = query.mappings().all()
         return exercises
 
-    @staticmethod
-    async def get_photos_exercise_by_id(session: AsyncSession, exercise_id: int):
-        photos_exercise = await session.execute(
+    async def get_photos_exercise_by_id(self, exercise_id: int):
+        photos_exercise = await self.session.execute(
             select(Exercise_photo).filter(Exercise_photo.exercise_id == exercise_id))
         result_photos_exercise = photos_exercise.mappings().all()
         return result_photos_exercise
 
-    @staticmethod
-    async def get_photos_by_ids(session: AsyncSession, photo_ids: list[int]):
+    async def get_photos_by_ids(self, photo_ids: list[int]):
         query = select(Exercise_photo).filter(Exercise_photo.id.in_(photo_ids))
-        result = await session.execute(query)
+        result = await self.session.execute(query)
         photos = result.mappings().all()
         return photos
 
     # TODO перенести в слов сервиса т.к. это работа с файловой системой
-    @staticmethod
-    async def save_photos(session: AsyncSession, exercise_id: int, exercise_name: str, photos: list):
+    async def save_photos(self, exercise_id: int, exercise_name: str, photos: list):
         for photo in photos:
             photo.filename = photo.filename.lower()
             path_photo = f"src/media/Photos_exercise/{exercise_id}_{exercise_name}_{uuid4()}.png"
@@ -229,15 +227,19 @@ class ExerciseRepository:
                 data = await photo.read()
                 await buffer.write(data)
             add_photo = insert(Exercise_photo).values(photo=path_photo[4:], exercise_id=exercise_id)
-            await session.execute(add_photo)
+            await self.session.execute(add_photo)
 
-    @staticmethod
-    async def delete_created_exercise_by_id(session: AsyncSession, exercise_id: int):
-        await session.execute(delete(Exercise).filter(Exercise.id == exercise_id))
+    async def delete_created_exercise_by_id(self, exercise_id: int):
+        try:
+            await self.session.execute(delete(Exercise).filter(Exercise.id == exercise_id))
+        except IntegrityError as exc:
+            log.exception(exc)
 
-    @staticmethod
-    async def delete_photos_by_ids(session: AsyncSession, photo_ids: list[int]):
-        await session.execute(delete(Exercise_photo).filter(Exercise_photo.id.in_(photo_ids)))
+    async def delete_photos_by_ids(self, photo_ids: list[int]):
+        try:
+            await self.session.execute(delete(Exercise_photo).filter(Exercise_photo.id.in_(photo_ids)))
+        except IntegrityError as exc:
+            log.exception(exc)
 
 
 class SetRepository:
