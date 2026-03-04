d10129870c49e2b83c6906a441dece1e8a06c100
l-Spook-l <uaspookua@gmail.com>
2026-03-04 22:09:11 +0200

    feat: introduce structured error handling with custom exceptions

--- src/workouts/service.py
+++ src/workouts/service.py
@@ -1,8 +1,13 @@
 import os
+from pathlib import Path
+from uuid import uuid4
 
-from fastapi import UploadFile, HTTPException
+import aiofiles
+from fastapi import UploadFile
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.core.exceptions.custom_exceptions import NotFoundError, WorkoutCreateError, BadRequestError, \
+    PermissionDeniedError, AppError
 from src.workouts.models import Exercise
 from src.workouts.repository import ExerciseRepository, WorkoutRepository, SetRepository
 from src.workouts.schemas import WorkoutCreate, WorkoutUpdate, ExerciseUpdate, SetUpdate, SetCreate
@@ -18,8 +23,9 @@ class WorkoutService:
             workout_id = await self.repo.create_workout(form)
             await self.repo.session.commit()
             return workout_id
-        except WorkoutCreateError:
+        except Exception as e:
             await self.repo.session.rollback()
+            raise WorkoutCreateError("Can't create workout") from e
 
     async def get_filtered_workouts(
             self,
@@ -48,29 +54,35 @@ class WorkoutService:
 
     async def add_user_workout_association(self, user, user_id, workout_id):
         if user.id == user_id:
-            raise HTTPException(status_code=400, detail="This workout cannot be added to the workout creator")
+            raise BadRequestError(detail="This workout cannot be added to the workout creator")
 
         result_existing = await self.repo.get_user_workout_association(user_id, workout_id)
         if result_existing.scalar():
-            raise HTTPException(status_code=400, detail="This workout is already added to the user")
+            raise BadRequestError(detail="This workout is already added to the user")
 
         association_created = await self.repo.add_user_workout_association(user_id, workout_id)
         if not association_created:
-            raise HTTPException(status_code=404, detail="User or Workout not found")
+            raise NotFoundError(detail="User or Workout not found")
 
         await self.repo.session.commit()
 
     async def get_one_workout(self, workout_id: int, user_id: int = None):
         workout = await self.repo.get_one_workout(workout_id)
+        if not workout:
+            raise NotFoundError(detail="Workout not found")
+
         if not workout.is_public and user_id != workout.user_id:
-            raise HTTPException(status_code=403)
+            raise PermissionDeniedError(detail="Access denied")
+
         return workout
 
     async def get_active_workout(self, workout_id: int, user_id: int):
         workout = await self.get_one_workout(workout_id, user_id)
+
         association_query_result = await self.repo.get_active_workout(workout_id, user_id)
         if not association_query_result.first() and workout.user_id != user_id:
-            raise HTTPException(status_code=403)
+            raise PermissionDeniedError(detail="Access denied")
+
         return workout
 
     async def get_user_added_workouts(
@@ -84,7 +96,7 @@ class WorkoutService:
     ):
         user = await user_repo.get_user_by_id(session, user_id)
         if not user:
-            raise HTTPException(status_code=404, detail="User not found")
+            raise NotFoundError(detail="User not found")
 
         workouts = await self.repo.get_user_added_workouts(
             user_id=user_id,
@@ -105,13 +117,14 @@ class WorkoutService:
         workout_difficulties = await self.repo.get_workout_difficulties()
         return workout_difficulties
 
-    async def update_workout(
-            self,
-            workout_id: int,
-            update_data: WorkoutUpdate
-    ):
-        await self.repo.update_workout(workout_id, update_data)
-        await self.repo.session.commit()
+    async def update_workout(self, workout_id: int, update_data: WorkoutUpdate):
+        try:
+            workout = await self.repo.update_workout(workout_id, update_data)
+            await self.repo.session.commit()
+            return workout
+        except AppError as e:
+            await self.repo.session.rollback()
+            raise e
 
     async def delete_created_workout(self, workout_id: int):
         workout = await self.repo.get_workout_by_id(workout_id)
@@ -154,12 +167,16 @@ class ExerciseService:
         return exercise_id
 
     async def add_new_photos_exercise(self, exercise_id: int, exercise_name: str, photos: list):
-        exercise = await self.repo.session.get(Exercise, exercise_id)  # TODO - а тут ли оно должно быть?
-        if not exercise:
-            raise HTTPException(status_code=404, detail="Exercise not found")
-        if photos:
-            await self.repo.save_photos(exercise_id, exercise_name, photos)
-        await self.repo.session.commit()
+        try:
+            exercise = await self.repo.session.get(Exercise, exercise_id)  # TODO - а тут ли оно должно быть?
+            if not exercise:
+                raise NotFoundError(detail="Exercise not found")
+            if photos:
+                await self._save_photos(exercise_id, exercise_name, photos)
+            await self.repo.session.commit()
+        except Exception as e:
+            await self.repo.session.rollback()
+            raise BadRequestError(f"Unexpected error: {str(e)}")
 
     async def update_exercise(self, exercise_id: int, update_data: ExerciseUpdate):
         if update_data.video:
@@ -186,11 +203,11 @@ class ExerciseService:
     async def delete_photo(self, exercise_id: int, photo_ids: list[int]):
         exercise = await self.repo.get_exercise_by_id(exercise_id=exercise_id)
         if not exercise:
-            raise HTTPException(status_code=404, detail="Exercise not found")
+            raise NotFoundError(detail="Exercise not found")
 
         photos = await self.repo.get_photos_by_ids(photo_ids=photo_ids)
         for photo in photos:
-            photo_path = f"src/{photo["Exercise_photo"].photo}"
+            photo_path = f"src/{photo['Exercise_photo'].photo}"
             if os.path.exists(photo_path):
                 os.remove(photo_path)
 
@@ -203,18 +220,29 @@ class SetService:
         self.repo = repo
 
     async def create_set(self, number_sets: int, data: SetCreate):
-        new_set = await self.repo.create_set(number_sets=number_sets, data=data)
-        await self.repo.session.commit()
-        return new_set
+        try:
+            await self.repo.create_set(number_sets=number_sets, data=data)
+            await self.repo.session.commit()
+        except Exception as e:
+            await self.repo.session.rollback()
+            raise BadRequestError(f"Unexpected error: {str(e)}")
 
     async def get_sets(self, user_id: int, exercise_ids: list[int]):
         sets = await self.repo.get_sets(user_id=user_id, exercise_ids=exercise_ids)
         return sets
 
     async def update_set(self, set_id: int, data: SetUpdate):
-        await self.repo.update_set(set_id=set_id, update_data=data)
-        await self.repo.session.commit()
+        try:
+            await self.repo.update_set(set_id=set_id, update_data=data)
+            await self.repo.session.commit()
+        except Exception as e:
+            await self.repo.session.rollback()
+            raise BadRequestError(f"Unexpected error: {str(e)}")
 
     async def delete_set(self, exercise_id: int, user_id: int):
-        await self.repo.delete_set(exercise_id=exercise_id, user_id=user_id)
-        await self.repo.session.commit()
+        try:
+            await self.repo.delete_set(exercise_id=exercise_id, user_id=user_id)
+            await self.repo.session.commit()
+        except Exception as e:
+            await self.repo.session.rollback()
+            raise BadRequestError(f"Unexpected error: {str(e)}")
